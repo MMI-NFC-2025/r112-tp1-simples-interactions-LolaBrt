ba26a4426f10bf8ef9b8eefff74a63068053b8e5
LolaBrt <lola.brouart@edu.univ-fcomte.fr>
2025-11-07 14:21:57 +0100

    commit3

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,6 @@ const rouge = document.querySelector("#a-mettre-en-rouge");
 rouge.style.color = "red";
 
 const button = document.querySelector("#en-rouge-suite-a-click");
-button.addEventListener("click", () => {
+button.addEventListener("click", (evt) => {
     rouge.style.color = "red";
 });
